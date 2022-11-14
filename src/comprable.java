73add5532d371817a1d29a39feee6b7c4b9982a8
Gabriel Marin <luisgmmh18v1@gmail.com>
2022-11-14 01:40:08 -0500

    jjhhjjj

---
+++ src/comprable.java
@@ -0,0 +1,22 @@
+public abstract class comprable {
+    private double precio;
+    private double peso;
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public abstract void show();
+}
